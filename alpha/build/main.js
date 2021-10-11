b0cf9f91833afbc49632f5bbfb16f93c05021888
lingan233 <71675451+lingan233@users.noreply.github.com>
2021-10-11 12:15:17 -0400

    Add files via upload

--- alpha/build/main.js
+++ alpha/build/main.js
@@ -1,20 +1,2 @@
-var btnShrink = anime.timeline({
-    easing: 'easeInOutSine',
-    autoplay: false
-  });
-  
-  btnShrink
-    .btn({
 
-    })
-
-    btnEnlarge
-    .btn({
-
-    })
-
-  
-  document.querySelector('.btn').onclick = function() {
-    btnShrink.play();
-  };
   
